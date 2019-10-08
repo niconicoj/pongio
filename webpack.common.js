e61ddde4a6f9b625e41e465d9d9712ad039df21d
niconicoj <joulin.nicolas@gmail.com>
2019-10-08 20:58:29 +0200

    inital commit

---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    game: './src/client/index.ts',
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use:  [
+          'ts-loader',
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          }
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/client/html/index.html',
+    }),
+  ],
+};
